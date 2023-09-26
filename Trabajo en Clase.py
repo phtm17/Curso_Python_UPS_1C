336c933c8d5acd6af457ca8ceb48c767e9b0a321
phtm17 <146017417+phtm17@users.noreply.github.com>
2023-09-25 22:24:31 -0500

    Trabajo_25_09_2023
    Trabajo en clase 25/09/2023

---
+++ Trabajo en Clase.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 22:01:21 2023
+
+@author: pault
+"""
+print("Parte 1")
+
+print("Ejercicio 1")
+print(2+(4*5/2))
+
+print("Ejercicio 2")
+print((8/2)*(2+2))
+
+print("Ejercicio 3")
+print(7-3*(2)**2)
+
+print("Ejercicio 4")
+print((5+3)*2-(6/3))
+
+print("Ejercicio 5")
+print((6*2)%5)
+
+print("Ejercicio 6")
+print(2**(2**3))
+
+print("Ejercicio 7")
+print(((((1+1)-1)*1)/1))
+
+print("Ejercicio 8")
+print(3*(4+2-2)/8)
+
+print("Ejercicio 9")
+print(((2+3)**2))
+
+print("Ejercicio 10")
+print((((4-2)*(3+1)/2)))
+
+print("Parte 2")
+print("Ejercicio 1")
+
+rebanadas=5*8
+consumo=4*(2)
+sobran=rebanadas-consumo
+print("Sobra",str(sobran),"rebanadas")
+
+print("Ejercicio 2")
+p=0.80*(4*20)
+print("Debe pagar $",str(p), "USD")
+
+print("Ejercicio 3")
+distancia=800/4
+print("En una hora recorre",str(distancia),"km")
+
+print("Ejercicio 4")
+pagar=0.9*(2*15+50)
+print("Se paga con la promoción $",str(pagar), "USD")
+
+print("Ejercicio 5")
+paginas=500/5
+print("Se deben leer",str(paginas),"paginas por dia ")
+
+print("Ejercicio 6")
+cuota=250/12
+print("Se debe pagar $",str(cuota),"USD")
+
+print("Ejercicio7")
+estudiante=3*(0.85*50)
+print("El costo final del pasaje es $",str(estudiante),"USD")
+
+print("Ejercicio 8")
+porciones=3*(500/5)
+print("Se necesita",str(porciones),"gramos de fideo")
+
+print("Ejercicio 9")
+galletas=24/6
+print("Le corresponde",str(galletas),"galletas a cada uno")
+
+print("Ejercicio 10")
+saldo=200+50*12
+print("Saldo despues de un año: $",str(saldo),"USD")
